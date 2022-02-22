6cbccb8feb23afdcd3f8f4e5a37edda8baa67252
rezamandaa <rezamandaa@gmail.com>
2022-02-23 02:33:17 +0700

    Update Flutter Basic

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    var fitWidth = BoxFit.fitWidth;
     return MaterialApp(
       title: 'Flutter Use Widget',
       theme: ThemeData(
@@ -40,6 +41,41 @@ class MyApp extends StatelessWidget {
               ),
             ],
           ),
+          Stack(
+            children: <Widget>[
+              Container(
+                color: Colors.purple[400],
+                alignment: Alignment.bottomLeft,
+                padding: const EdgeInsets.all(16.0),
+                child: const Text(
+                  "Transfer",
+                  style: TextStyle(fontSize: 15),
+                ),
+                width: 500.0,
+                height: 300.0,
+              ),
+              Container(
+                color: Colors.white,
+                alignment: Alignment.bottomCenter,
+                padding: const EdgeInsets.all(16.0),
+                child: const Text(
+                  "Costa Mendekat Ke Palmairas",
+                  style: 
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                width: 500.0,
+                height: 250.0,
+              ),
+               
+               SizedBox(
+                child: Image.network( "https://eu-images.contentstack.com/v3/assets/bltcc7a7ffd2fbf71f5/blt3dbe9d4135f085ef/60da8a26467112384c77686e/8e9f68dc9178d045468e572aefae38ffe9bf117a.jpg?auto=webp&format=pjpg&quality=80&width=1024",
+                width: 500.0,
+                height: 250.0,
+                fit: fitWidth,
+                ),
+              )
+            ],
+          )
         ],
       ),
       ),
