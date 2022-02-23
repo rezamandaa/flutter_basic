407d0d39d061acef90a7e8c3927c0c03bebba044
rezamandaa <rezamandaa@gmail.com>
2022-02-23 20:04:24 +0700

    update flutter basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,138 +1,127 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  
   @override
   Widget build(BuildContext context) {
-    var fitWidth = BoxFit.fitWidth;
     return MaterialApp(
-      title: 'Flutter Use Widget',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.red,
-      ),
-      home: Scaffold(appBar: AppBar(
-        title: const Text('MyApp'),
-      ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.all(20),
-                child: const Text('BERITA TERBARU',
-                  style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),),
-              ),
-              SizedBox(
-                width: 40,
-              ),
-              Container(
-                margin: EdgeInsets.all(20),
-                child: const Text('PERTANDINGAN HARI INI',
-                  style: TextStyle(
-                    fontSize:13, fontWeight: FontWeight.w600 )),
-              ),
-            ],
+      title: 'Contoh Date Picker',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("My App"),
+            backgroundColor: Colors.red,
           ),
-          Stack(
+          body: ListView(
             children: <Widget>[
-              Container(
-                color: Colors.purple[400],
-                alignment: Alignment.bottomLeft,
-                padding: const EdgeInsets.all(16.0),
-                child: const Text(
-                  "Transfer",
-                  style: TextStyle(fontSize: 15),
-                ),
-                width: 500.0,
-                height: 300.0,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: const Text("BERITA TERBARU"),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: const Text("PERTANDINGAN HARI INI"),
+                  ),
+                ],
+              ),
+              Stack(
+                children: <Widget>[
+                  
+                  Row(
+                    children: [
+                      Expanded(child:
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                          decoration: BoxDecoration(border: Border.all(color: Colors.purpleAccent, width: 4)),
+                          child: Image.network("https://akcdn.detik.net.id/community/media/visual/2019/04/08/b61401bc-e2fc-483c-9d0e-a5639194f37f_169.jpeg?w=700&q=90",
+                          width: 400,
+                          height: 300,
+                          fit: BoxFit.fitWidth)
+                        ),
+                      )  
+                    ],
+                  ),
+                  
+                  Row(
+                    children: [
+                      Expanded(child:
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                          decoration: BoxDecoration(border: Border.all(color: Colors.purpleAccent, width: 4)),
+                          alignment: Alignment.bottomCenter,
+                          child: const Text("Costa Mendekat ke Palmeiras", 
+                            style: TextStyle(
+                              fontSize: 30, 
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(20), 
+                        ),
+                      ),
+                    ],
+                  ),
+                                   Row(
+                    children: [
+                      Expanded(child: 
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(16, 0, 16, 8),
+                          decoration: const BoxDecoration(color: Colors.purpleAccent),
+                          child: const Text("Transfer", 
+                            style: TextStyle(
+                              fontSize: 20, 
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
               Container(
-                color: Colors.white,
-                alignment: Alignment.bottomCenter,
-                padding: const EdgeInsets.all(16.0),
-                child: const Text(
-                  "Costa Mendekat Ke Palmairas",
-                  style: 
-                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                margin: EdgeInsets.only(top: 15),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.grey)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Expanded(
+                        child: Image(
+                      image: NetworkImage(
+                          "https://akcdn.detik.net.id/community/media/visual/2020/10/21/gerard-pique_169.jpeg?w=700&q=90"),
+                    )),
+                    const VerticalDivider(width: 1.0),
+                    Expanded(
+                        child: Center(
+                            child: Container(
+                      margin: const EdgeInsets.only(left: 8),
+                      child: const Text(
+                          "Pique Bilang Wasit untungkan Madrid,Koemon Tepok Jidat"),
+                    ))),
+                  ],
                 ),
-                width: 500.0,
-                height: 250.0,
               ),
-               
-               SizedBox(
-                child: Image.network( "https://eu-images.contentstack.com/v3/assets/bltcc7a7ffd2fbf71f5/blt3dbe9d4135f085ef/60da8a26467112384c77686e/8e9f68dc9178d045468e572aefae38ffe9bf117a.jpg?auto=webp&format=pjpg&quality=80&width=1024",
-                width: 500.0,
-                height: 210.0,
-                fit: fitWidth,
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.grey)),
+                child: Row(
+                  children: const <Widget>[
+                    Text(
+                      "Barcelona Feb 13,2021",
+                      style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.normal),
+                    )
+                  ],
                 ),
-              )
+              ),
+
             ],
-          )
-        ],
-      ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
- 
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-   
-    return Scaffold(
-      appBar: AppBar(
-       
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+          )),
     );
   }
 }
